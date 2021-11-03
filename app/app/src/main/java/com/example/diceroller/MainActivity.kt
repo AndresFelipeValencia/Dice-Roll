4e02d78c2fb58c687ce0e088d88af9b427149e24
My AnFeVaGa <Job.andresvalencia@gmail.com>
2021-11-03 15:59:36 -0500

    Functionality of the dice, with two different results, displayed at the same time.

--- app/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
-
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,13 +17,34 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
             rollDice()
+            rollDiceOne()
         }
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
     }
+
+    private fun rollDiceOne() {
+        // Create new Dice object with 6 sides and roll it
+        val diceOne = Dice(6)
+        val diceRollOne = diceOne.roll()
+
+        // Update the screen with the dice roll
+        val resultTextViewOne: TextView = findViewById(R.id.textView2)
+        resultTextViewOne.text = diceRollOne.toString()
+    }
+
 }
+
+
+
